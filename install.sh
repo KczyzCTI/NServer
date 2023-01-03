a9aba92f9182630c0441c34f7e1df7929ac37820
KczyzCTI <235768@edu.p.lodz.pl>
2023-01-03 18:37:35 +0100

    Corrected install.sh script + should be called from CMake now Removed test.cpp target

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Installing required packages..."
 sudo apt update --yes &&
-sudo apt install pkg-config cmake libboost-all-dev dot doxygen pthreads --yes &&
+sudo apt install git gdb gcc g++ cmake --yes
+sudo apt install pkg-config libboost-all-dev doxygen graphviz --yes &&
 sudo apt install --no-install-recommends libasio-dev --yes
-
