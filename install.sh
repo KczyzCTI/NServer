022745d90aa1fe4a4a1ea64fd4a1d921c47495fe
KczyzCTI <235768@edu.p.lodz.pl>
2023-01-03 21:02:03 +0100

    .

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo "Installing required packages..."
 sudo apt update --yes &&
-sudo apt install git gdb gcc g++ cmake --yes
+sudo apt install gdb gcc
+sudo apt install g++ --yes
 sudo apt install pkg-config libboost-all-dev doxygen graphviz --yes &&
 sudo apt install --no-install-recommends libasio-dev --yes
